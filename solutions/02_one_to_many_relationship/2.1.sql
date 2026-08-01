12b842e4e2da25c1a777c482fa2156341dd1d2d8
cabralgabriel <gabrielcabralv@hotmail.com>
2026-08-01 07:21:14 -0300

    feat: add 2.1 solution

---
+++ solutions/02_one_to_many_relationship/2.1.sql
@@ -0,0 +1,11 @@
+-- 2.1 Report the account representative for each customer.
+
+SELECT
+  c.customerName AS customer_name,
+  CONCAT(e.firstName, ' ', e.lastName) AS represetantive_name
+FROM
+  Customers AS c
+JOIN
+  Employees AS e
+ON
+  c.salesRepEmployeeNumber = e.employeeNumber;
